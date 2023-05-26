d3a3ae6cc4daa0eb2517ed779ecd06161fc8e2f6
unknown <guicardosodeoliveira@gmail.com>
2023-05-26 17:18:23 -0300

    Resolução do Ex 10

--- 10/index.js
+++ 10/index.js
@@ -1,5 +1,8 @@
 // Crie sua função aqui
-
+function secondBiggestNumber(array) {
+    const sortedArray = array.sort();
+    return sortedArray.at(-2)
+}
 
 // Não altere as linhas abaixo
 console.log(secondBiggestNumber([0, 1, 2, 3, 4, 5]))
