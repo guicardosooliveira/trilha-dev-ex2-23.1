675a0ee81f6b139650d053758a65e4734e5f02c1
unknown <guicardosodeoliveira@gmail.com>
2023-05-26 16:08:07 -0300

    Resolução do Ex 4

--- 04/index.js
+++ 04/index.js
@@ -1,5 +1,11 @@
 // Crie sua função aqui
-
+function oneToNumber(number) {
+    const array = []
+    for (let i = 0; i < number; i++) {
+        array.push(i+1)
+    }
+    return array
+}
 
 // Não altere as linhas abaixo
 console.log(oneToNumber(10))
