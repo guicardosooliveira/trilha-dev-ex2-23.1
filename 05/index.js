52ff501b5973334c3543d940f1832b1d6e2e7703
unknown <guicardosodeoliveira@gmail.com>
2023-05-26 16:48:44 -0300

    Resolução do Ex 5

--- 05/index.js
+++ 05/index.js
@@ -1,5 +1,12 @@
 // Crie sua função aqui
+function evenArray(number) {
+    const evenArray = []
 
+    for (let i = 0; i <= number; i += 2) {
+        evenArray.push(i)
+    }
+    return evenArray
+}
 
 // Não altere as linhas abaixo
 console.log(evenArray(10))
